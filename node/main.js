ff51af95f9a3d28a89f24bc5933f2d107754b064
jva411 <j.victoralves411@gmail.com>
2019-10-17 09:35:31 -0300

    .

--- node/main.js
+++ node/main.js
@@ -3,13 +3,13 @@ const yts = require('yt-search')
 
 let search = "morro do dende"
 
-yts(search, (err, res) => {
+yts(search, async (err, res) => {
     if(!err){
         let video = res.videos[0]
         console.log(video)
-        console.log(ytdl.validateURL(video.url))
-        ytdl.getInfo(`${video.videoId}`, (err, info) => {
-            console.log(info)
+        await ytdl.getInfo(`${video.videoId}`, (err, info) => {
+            console.log(info.player_response.videoDetails.thumbnail.thumbnails.pop())
+            console.log(info.author)
         })
     }
 })
