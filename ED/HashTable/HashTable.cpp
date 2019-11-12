17475f36d0ad628239f268941f186fb8395a36ae
jva411 <j.victoralves411@gmail.com>
2019-11-12 16:24:56 -0300

    .

--- ED/HashTable/HashTable.cpp
+++ ED/HashTable/HashTable.cpp
@@ -1,8 +1,20 @@
 #include <string.h>
 
 template <typename K, typename V>
-struct HashTable {
+class HashTable {
+
+    private: class Node{
+        K *key;
+        V *value;
+    }
+
+    Node *nodes[];
+    int n, min;
+
     public: void init(){
-        
+        n = 0;
+        min = 10;
+        nodes = new *Node[min];
+        for(int i=0; i<min; i++) nodes[i] = 0;
     }
 };
--- ED/Heap/Heap.c
+++ ED/Heap/Heap.c
@@ -2,6 +2,23 @@
 
 typedef struct Heap {
 
-    
+    int* vetor, n, min;
 
 } Heap;
+
+Heap *criarHeap(){
+    Heap *hp = (Heap*) malloc(sizeof(Heap));
+    hp->min = 10;
+    hp->n = 0;
+    hp->vetor = (int*) malloc(sizeof(int) * hp->min);
+    return hp;
+}
+
+void rise(Heap *hp, int index){
+
+}
+
+void add(Heap *hp, int x){
+    hp->vetor[hp->n] = x;
+    rise(hp, hp->n++);
+}
